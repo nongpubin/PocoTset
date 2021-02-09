8614b9a5449c4a33a0b8de0684aae00a97bd79e5
nongpubin <1050416698@qq.com>
2021-02-09 23:45:28 +0800

    unix线程

---
+++ Unix/thread.c
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-09 21:58:19
+ * @LastEditTime: 2021-02-09 23:42:13
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \npb\Unix\thread.c
+ */
+
+#include "stdio.h"
+
+int main() 
+{
+    printf("hello !!! \r\n");
+    return 0;
+}
